2562ea2e9fddd56bccce0c49ee713ee0105b8055
Beloin Sena <beloin.rodrigues@gmail.com>
2024-11-15 01:22:08 -0300

    WIP - Creating workqueue

--- driver.c
+++ driver.c
@@ -1,24 +1,25 @@
-
-#include <linux/module.h>
-#include <linux/kernel.h>
+#include "linux/workqueue.h"
 #include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
 
 #define DRIVER_AUTHOR "Beloin <beloin.rodrigues@gmail.com>"
-#define DRIVER_DESC   "A simple Workqueue driver"
+#define DRIVER_DESC "A simple Workqueue driver"
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+
+// Already existing defines
+// #define create_workqueue(name) alloc_workqueue("%s", WQ_MEM_RECLAIM, 1, (name))
+// #define create_singlethread_workqueue(name) alloc_workqueue("%s", WQ_UNBOUND | WQ_MEM_RECLAIM, 1, (name))
+
+DECLARE_WORK(o, f)
 
 int init_module(void) {
+  struct workqueue_struct * myqw= create_workqueue("beloinswq");
   printk(KERN_INFO "Initializing Workqueue register");
   return 0;
 }
 
-
-void cleanup_module(void)
-{
-  printk(KERN_INFO "Ending Workqueue register");
-}
-
-// void register_wq(){
-//   alloc_workqueue("%s", WQ_MEM_RECLAIM, 1, (name))
-// }
+void cleanup_module(void) { printk(KERN_INFO "Ending Workqueue register"); }
