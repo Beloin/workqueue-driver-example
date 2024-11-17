1c972d2e026e9a0e9619976b09d02a57bb640de5
Beloin Sena <beloin.rodrigues@gmail.com>
2024-11-16 21:40:08 -0300

    WIP - Creating driver

--- driver.c
+++ driver.c
@@ -11,15 +11,20 @@ MODULE_AUTHOR(DRIVER_AUTHOR);
 MODULE_DESCRIPTION(DRIVER_DESC);
 
 // Already existing defines
-// #define create_workqueue(name) alloc_workqueue("%s", WQ_MEM_RECLAIM, 1, (name))
-// #define create_singlethread_workqueue(name) alloc_workqueue("%s", WQ_UNBOUND | WQ_MEM_RECLAIM, 1, (name))
+// #define create_workqueue(name) alloc_workqueue("%s", WQ_MEM_RECLAIM, 1,
+// (name)) #define create_singlethread_workqueue(name) alloc_workqueue("%s",
+// WQ_UNBOUND | WQ_MEM_RECLAIM, 1, (name))
 
-DECLARE_WORK(o, f)
+static void workqueue_fn(struct work_struct *work);
+
+static DECLARE_WORK(work, workqueue_fn);
 
 int init_module(void) {
-  struct workqueue_struct * myqw= create_workqueue("beloinswq");
+  struct workqueue_struct *myqw = create_workqueue("beloinswq");
   printk(KERN_INFO "Initializing Workqueue register");
   return 0;
 }
 
 void cleanup_module(void) { printk(KERN_INFO "Ending Workqueue register"); }
+
+static void workqueue_fn(struct work_struct *work) {}
